cd641b102d800167b48d15a4f4ad6fdf77a3a8ac
iumehara <iumehara10@gmail.com>
2013-06-12 20:59:35 -0400

    updated to ruby 2.0.0 rails 4.0.0.rc2

--- db/schema.rb
+++ db/schema.rb
@@ -9,29 +9,29 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612141011) do
+ActiveRecord::Schema.define(version: 20130612141011) do
 
-  create_table "gifts", :force => true do |t|
+  create_table "gifts", force: true do |t|
     t.integer  "giver_id"
     t.integer  "receiver_id"
     t.integer  "provider_id"
     t.string   "status"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "providers", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "providers", force: true do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.boolean  "giver"
     t.boolean  "receiver"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
